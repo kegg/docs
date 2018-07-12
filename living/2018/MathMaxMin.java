b0d3e9c91caf350cd7cfc89e31b13a5501bb5d66
Kyle Eggleston <kyle@dreamsense.com>
2018-07-12 09:41:24 -0600

    Added jul 12, 2018

---
+++ living/2018/MathMaxMin.java
@@ -0,0 +1,13 @@
+public class MathMaxMin {
+
+  public static void main(String[] args) {
+    int a = Math.min(10, 4);
+    System.out.print("min(10, 4): ");
+    System.out.println(a);
+
+    int b = Math.max(10, 4);
+    System.out.print("max(10, 4): ");
+    System.out.println(b);
+  }
+
+}
